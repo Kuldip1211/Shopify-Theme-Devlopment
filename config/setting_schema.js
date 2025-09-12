148dc6fb21c38f8ab5f14a3ead1256f7703baccb
Kuldeep Chudasma <kuldeep.chudasma@pc0695.radixweb.local>
2025-09-12 16:50:40 +0530

    third commit

---
+++ config/setting_schema.js
@@ -0,0 +1,9 @@
+[
+    {
+        "name" : "theme_info",
+        "theme_name" : "theme_from_scratch",
+        "theme_version" : "1.0.0",
+        "theme_author" : "Kuldeep Chudasama",
+        "theme_description" : "A custom theme built from scratch."
+    }
+]
